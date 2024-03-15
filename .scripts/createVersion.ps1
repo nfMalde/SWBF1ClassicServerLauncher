6e199aa0c1315b3576726872a07c8a579ba6bac6
Malte Peters <malte@networkfreaks.de>
2024-03-15 20:01:32 +0100

    Fixed versioning

--- .scripts/createVersion.ps1
+++ .scripts/createVersion.ps1
@@ -4,12 +4,23 @@ $filteredTags = $tags -match '^v\d+\.\d+'
 
 # Sort the filtered tags in version order
 $sortedTags = $filteredTags | Sort-Object { [Version]$_.TrimStart("v") } -Descending
-$highestTag  = "v1.0.0"
+$highestTag  = [Version]"1.0.0"
 if ($sortedTags) {
     # Select the highest tag
-    $highestTag = $sortedTags[0]
+    $highestTag = [Version] $sortedTags[0].TrimStart("v")
+    $major,$minor,$rev = "$($highestTag)".Split('.')
+
+    if (!$rev) {
+        $rev = -1
+    }
  
 }
 
-Write-Host "newVersion=$($highestTag.TrimStart("v"))" | Out-File -FilePath $Env:GITHUB_OUTPUT -Encoding utf8 -Append
+
+
+$rev = [int]$rev + 1
+
+$highestTag = "$($major).$($minor).$($rev)"  
+
+Write-Host "newVersion=$($highestTag)" | Out-File -FilePath $Env:GITHUB_OUTPUT -Encoding utf8 -Append
 
