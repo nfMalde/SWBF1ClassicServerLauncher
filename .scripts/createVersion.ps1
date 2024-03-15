10a900c67954e5ffc555a5bd7f1ae237f6f623d9
Malte Peters <malte@networkfreaks.de>
2024-03-15 20:03:38 +0100

    Fixed Version File

--- .scripts/createVersion.ps1
+++ .scripts/createVersion.ps1
@@ -4,23 +4,23 @@ $filteredTags = $tags -match '^v\d+\.\d+'
 
 # Sort the filtered tags in version order
 $sortedTags = $filteredTags | Sort-Object { [Version]$_.TrimStart("v") } -Descending
-$highestTag  = [Version]"1.0.0"
+$highestTag = [Version]"1.0.0"
 if ($sortedTags) {
     # Select the highest tag
     $highestTag = [Version] $sortedTags[0].TrimStart("v")
-    $major,$minor,$rev = "$($highestTag)".Split('.')
+    $major, $minor, $rev = "$($highestTag)".Split('.')
 
     if (!$rev) {
         $rev = -1
     }
  
+
+    $rev = [int]$rev + 1
+
+    $highestTag = "$($major).$($minor).$($rev)"  
 }
 
 
-
-$rev = [int]$rev + 1
-
-$highestTag = "$($major).$($minor).$($rev)"  
-
-Write-Host "newVersion=$($highestTag)" | Out-File -FilePath $Env:GITHUB_OUTPUT -Encoding utf8 -Append
+Write-Host "newVersion=$($highestTag)"
+#Write-Host "newVersion=$($highestTag)" | Out-File -FilePath $Env:GITHUB_OUTPUT -Encoding utf8 -Append
 
